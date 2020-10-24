140c438d9c16c5feced6368fc682710ac2b69c93
bradchao <brad@brad.tw>
2020-10-24 09:57:14 +0800

    3

--- app/src/main/java/tw/org/tcca/apps/test7/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test7/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test1(View view) {
+        webView.loadUrl("javascript:test1()");
     }
 
     private class MyWebViewClient extends WebViewClient {
