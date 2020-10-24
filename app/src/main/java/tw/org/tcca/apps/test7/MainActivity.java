fb9c11b1f11087da5f16fd5c49131bb56307138f
bradchao <brad@brad.tw>
2020-10-24 11:27:50 +0800

    9

--- app/src/main/java/tw/org/tcca/apps/test7/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test7/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -15,6 +20,7 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private LocationManager lmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
         if (ContextCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+                PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            }, 123);
         } else {
             init();
         }
@@ -33,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 123 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 123 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                grantResults[2] == PackageManager.PERMISSION_GRANTED){
             init();
         }else{
             finish();
@@ -41,8 +59,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
+        lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         webView = findViewById(R.id.webview);
         initWebView();
+    }
+
+    private MyListener myListener;
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myListener = new MyListener();
+        lmgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1*1000, 0, myListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (myListener != null) {
+            lmgr.removeUpdates(myListener);
+        }
+    }
+
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(@NonNull String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(@NonNull String provider) {
+
+        }
     }
 
     private void initWebView(){
