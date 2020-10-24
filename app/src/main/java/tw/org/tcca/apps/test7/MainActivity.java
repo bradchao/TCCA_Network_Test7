7647dc9f883b79b04e5b2c79d948631aaaf83e0d
bradchao <brad@brad.tw>
2020-10-24 09:45:01 +0800

    1

---
+++ app/src/main/java/tw/org/tcca/apps/test7/MainActivity.java
@@ -0,0 +1,37 @@
+package tw.org.tcca.apps.test7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        initWebView();
+    }
+
+    private void initWebView(){
+        webView.setWebViewClient(new MyWebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/brad.html");
+    }
+
+    public void test1(View view) {
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+    }
+}
